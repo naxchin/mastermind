15134d2d5ed7fc54e32ab15e69af1202e47cc3cc
Nathalie xiaodan CHIN <nachin@e1r10p2.clusters.42paris.fr>
2022-09-30 21:34:33 +0200

    to do : remove loop

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ print("| '_ ` _ \ / _` / __| __/ _ \ '__| '_ ` _ \| | '_ \ / _` |")
 print("| | | | | | (_| \__ \ ||  __/ |  | | | | | | | | | | (_| |")
 print("|_| |_| |_|\__,_|___/\__\___|_|  |_| |_| |_|_|_| |_|\__,_|", "\n")
 
-correct_number = random.randint(1000, 9999)
-print(correct_number)
-user_number = user_input()
-if (user_number == correct_number):
-	print('You win first try gg !', '\n')
-else:
-	numbers_list = [user_number, correct_number]
-	result = try_again_and_again(numbers_list)
+while(1):
+	correct_number = random.randint(1000, 9999)
+	print(correct_number)
+
+	user_number = user_input()
+
+	if (user_number == correct_number):
+		print("You win first try gg !")
+	else:
+		numbers = [user_number, correct_number]
+		result = try_again_and_again(numbers)
 
--- mastermind_utils.py
+++ mastermind_utils.py
@@ -1,31 +1,37 @@
 def user_input():
-	user_number = int(input('please enter a number with 4 digit:'))
+	user_number = int(input("please enter a number with 4 digit:"))
 	while user_number < 1000 or user_number > 9999 :
-		user_number = int(input('please re-enter a number, it must have 4 digit :'))
+		user_number = int(input("please re-enter a number, it must have 4 digit !"))
 	return user_number
 
 
-def	try_again_and_again(numbers_list):
-	user_number = str(numbers_list[0])
-	correct_number = str(numbers_list[1])
+def	try_again_and_again(numbers):
+	user_number = numbers[0]
+	correct_number = numbers[1]
 	nb_of_tries = 0
 	while (user_number != correct_number):
 		nb_of_tries += 1
-		count_correct_value = 0
+		count = 0
+		user_number = str(user_number)
+		correct_number = str(correct_number)
 		check_values = ['•', '•', '•', '•']
+
 		for i in range(0, 4, 1):
 			if (user_number[i] == correct_number[i]):
-				count_correct_value += 1
 				check_values[i] = user_number[i]
+				count += 1
 			else:
 				continue
-		if (count_correct_value == 0):
-			print('all input are wrongs sorry try again', '\n')
+	
+		if (count == 0):
+			print("all input are wrongs sorry try again")
 			user_number = user_input
-		elif (count_correct_value < 4) and (count_correct_value != 0):
-			print('the correct value(s) is/are :', '\n')
-			for i_array in check_values:
-				print(i_array, end=' ')
+		elif (count < 4) and (count != 0):
+			print("try again")
+			print("these/this value(s) was/were correct :", '\n')
+			for j in check_values:
+				print(j, end=' ')
 			print('\n')
 			user_number = user_input
-	return numbers_list
+
+		return numbers
